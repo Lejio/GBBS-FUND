6cdc6a6a8e1409fd6a2b9fd587e7d4e1d48a6384
Gene Ni <geneisjuan@gmail.com>
2024-09-22 22:46:40 -0400

    updated classnotes

---
+++ class-notes/week1/math/main.py
@@ -0,0 +1,25 @@
+
+a = 5
+b = 3
+
+c = a + b
+print(c)
+
+a = 4
+b = 2
+c = a - b
+print(c)
+
+a = 3.2
+b = 1.1
+c = a + b
+print(c)
+c = a - b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+c = a + " " + b
+print(c)
---
+++ class-notes/week1/variables/main.py
@@ -0,0 +1,28 @@
+a = 3 # integer
+
+b = 1.4 # float
+
+c = "Hello" # string
+
+d = True # boolean
+
+e = (1, 2, 3) # tuple
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+a = "Hello"
+b = "World"
+
+print(a)
+print(b)
+
